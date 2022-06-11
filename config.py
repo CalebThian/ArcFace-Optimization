a13fcb32ecfb18ab0b1f7186bff4b4a34175146a
CalebThian <calebthian@gmail.com>
2022-06-11 10:42:18 +0800

    model parameters setting

--- config.py
+++ config.py
@@ -24,9 +24,25 @@ class Config:
         T.Normalize(mean=[0.5], std=[0.5]),
     ])
     
-    # Parameters setting
+    # Training Parameters setting
     train_batch_size = 64
     test_batch_size = 60
 
     pin_memory = True  # if memory is large, set it True for speed
     num_workers = 4  # dataloader
+    
+    # Model Parameters setting
+    backbone = 'fmobile' # [resnet, fmobile]
+    metric = 'arcface'  # [cosface, arcface]
+    embedding_size = 512
+    drop_ratio = 0.5
+    
+    epoch = 30
+    optimizer = 'sgd'  # ['sgd', 'adam']
+    lr = 1e-1
+    lr_step = 10
+    lr_decay = 0.95
+    weight_decay = 5e-4
+    loss = 'focal_loss' # ['focal_loss', 'cross_entropy']
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    checkpoints = "checkpoints"
