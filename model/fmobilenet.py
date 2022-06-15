bf3a902d075bc2c12da97ec96dcff58ed98fb7d6
CalebThian <calebthian@gmail.com>
2022-06-15 20:02:07 +0800

    Create a new model myFaceMobileNet

--- model/fmobilenet.py
+++ model/fmobilenet.py
@@ -111,8 +111,42 @@ class FaceMobileNet(nn.Module):
         out = self.linear(out)
         out = self.bn(out)
         return out
-
-
+    
+class myFaceMobileNet(nn.Module):
+    def __init__(self, embedding_size):
+        super().__init__()
+        self.conv1 = ConvBnPrelu(1, 64, kernel=(3, 3), stride=2, padding=1)
+        self.conv2 = ConvBn(64, 64, kernel=(3, 3), stride=1, padding=1, groups=64)
+        self.conv3 = DepthWise(64, 64, kernel=(3, 3), stride=2, padding=1, groups=128)
+        self.conv4 = MultiDepthWiseRes(num_block=2, channels=64, kernel=3, stride=1, padding=1, groups=128)
+        self.conv5 = DepthWise(64, 128, kernel=(3, 3), stride=2, padding=1, groups=256)
+        self.conv6 = MultiDepthWiseRes(num_block=4, channels=128, kernel=(3, 3), stride=1, padding=1, groups=256)
+        self.conv7 = DepthWise(128, 128, kernel=(3, 3), stride=2, padding=1, groups=512)
+        self.conv8 = MultiDepthWiseRes(num_block=2, channels=128, kernel=(3, 3), stride=1, padding=1, groups=256)
+        self.conv9 = ConvBnPrelu(128, 512, kernel=(1, 1))
+        self.conv10 = ConvBn(512, 512, groups=512, kernel=(7, 7))
+        self.flatten = Flatten()
+        #由于我们的输入是1 x 128 x 128，经过多层卷积之后，其变成512 x 2 x 2，也就是2048。如果你不知道或者你懒得去算输入的图片经过卷积之后的维度是多少，你可以给网络传入一个假数据，报错信息会告诉你这个维度的值。
+        #另外，这里的embedding_size由外部传入，它表示用多大的向量来表示一张人脸。像 Facenet 是使用了128维的向量来表征一张人脸，我们这里使用512。
+        self.linear = nn.Linear(2048, embedding_size, bias=False)
+        self.bn = nn.BatchNorm1d(embedding_size)
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        out = self.conv6(out)
+        out = self.conv7(out)
+        out = self.conv8(out)
+        out = self.conv9(out)
+        out = self.conv10(out)
+        out = self.flatten(out)
+        out = self.linear(out)
+        out = self.bn(out)
+        return out
+    
 if __name__ == "__main__":
     from PIL import Image
     import numpy as np
@@ -122,7 +156,7 @@ if __name__ == "__main__":
     x = np.asarray(x, dtype=np.float32)
     x = x[None, None, ...]
     x = torch.from_numpy(x)
-    net = FaceMobileNet(512)
+    net = myFaceMobileNet(512)
     net.eval()
     with torch.no_grad():
         out = net(x)
